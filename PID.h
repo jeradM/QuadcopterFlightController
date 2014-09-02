09a1b1dcbe95556f16e6d491ccca4a513f958d00
Jerad Meisner <jerad.meisner@gmail.com>
2014-09-01 23:09:12 -0700

    Initial Commit (PID.h)

---
+++ PID.h
@@ -0,0 +1,67 @@
+#ifndef __PID_H__
+#define __PID_H__
+
+class PID {
+public:
+  PID(float init_p = 0.0, float init_i = 0.0, float init_d = 0.0, uint16_t init_i_limit = 0) {
+    _kP            = init_p;
+    _kI            = init_i;
+    _kD            = init_d;
+    _i_limit       = init_i_limit;
+    _d_prev        = NAN;
+  }
+  
+  float get_pid(float error);
+  
+  void operator () (float p, float i, float d, uint16_t i_limit) {
+    _kP      = p;
+    _kI      = i;
+    _kD      = d;
+    _i_limit = i_limit;
+  }
+  
+  float get_kP() {
+    return _kP;
+  }
+  
+  float get_kI() {
+    return _kI;
+  }
+  
+  float get_kD() {
+    return _kD;
+  }
+  
+  float get_i_limit() {
+    return _i_limit;
+  }
+  
+  void kP(float p) {
+    _kP = p;
+  }
+  
+  void kI(float i) {
+    _kI = i;
+  }
+  
+  void kD(float d) {
+    _kD = d;
+  }
+  
+  void i_limit(float i) {
+    _i_limit = i;
+  }
+  
+  
+private: 
+  float     _kP;
+  float     _kI;
+  float     _kD;
+  uint16_t  _i_limit;
+  float     _i_sum;
+  float     _err_prev;
+  float     _d_prev;
+  uint32_t  _t_prev;
+}
+
+#endif
