c4df40ab629be1320afa352e50cc0797e0410468
Jerad Meisner <jerad.meisner@gmail.com>
2014-09-02 00:06:45 -0700

    added PID.cpp

---
+++ pid.cpp
@@ -0,0 +1,45 @@
+#include "PID.h"
+
+#include <math.h>
+
+float PID::get_pid(float error) {
+  uint32_t cur_time = millis();
+  uint32_t dt       = cur_time - _t_prev;
+  float result      = 0.0;
+  float time_dif;
+   
+   if (_t_prev == 0 || dt > 1000) {
+     dt = 0;
+     _kI = 0;
+   }
+   
+   _t_prev = cur_time;
+   time_dif = (float)dt/1000.0f;
+   
+   result = error * _kP;
+   
+   if (_kI != 0 && dt > 0) {
+     _i_sum += error * _kI * time_dif;
+     
+     if (_i_sum < -_i_limit) _i_sum     = -_i_limit;
+     else if (_i_sum > _i_limit) _i_sum = _i_limit;  
+     
+     result += _i_sum;
+   }
+   
+   if (_kD != 0 && dt > 0) {
+     float dx;
+     
+     if (isNAN(_d_prev)) {
+       dx      = 0.0f;
+       _d_prev = 0.0f;
+     }
+     else {
+       dx = (error - _err_prev) / time_dif;
+     }
+     
+     result += dx * _kD;
+   }
+   
+   return result;
+}
